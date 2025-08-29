6d1cd694960599c51d03562947ab14557696bc87
rafael ferreira <rafael.ferreira@buttinimoraes.com.br>
2025-08-29 11:47:34 -0300

    removing test

--- worker_transmitter_loader/src/controller/files_loader_controller_test.py
+++
@@ -1,5 +0,0 @@
-from .files_loader_controller import FilesLoaderController
-
-def test_file_loader_controller():
-    files_loader_controller = FilesLoaderController()
-    files_loader_controller.load("Saídas/01.2025/SAI_47508411163705_012025.txt")
